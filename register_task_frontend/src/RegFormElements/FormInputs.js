cce4a0ff585deb5a7a125ee580d001d99087116a
Kris13081 <kristianstefanov0@gmail.com>
2023-08-28 08:30:09 +0300

    Add custom messages for the form inputs.

--- register_task_frontend/src/RegFormElements/FormInputs.js
+++ register_task_frontend/src/RegFormElements/FormInputs.js
@@ -5,20 +5,26 @@ import DateInputs from "./Date";
 import handleSubmit from "./SubmitHandler";
 
 function FormInputs() {
+
     return (
 		<form action="" className="form-container" onSubmit={handleSubmit}>
 		<div className="text-input-container">      
 			<div className="input-wrapper-firstname"> 
-				<span>First Name: </span><input type="text" name="firstName" required pattern="^[A-Za-z]{2,30}$" onInvalid= {(e) => { e.target.setCustomValidity("Length has to be between 2 and 30 characters, without digits and symbols!"); e.target.setCustomValidity("");}}/>
+				<span>First Name: </span>
+				<input type="text" name="firstName" pattern="^[A-Za-z]{2,25}$" onInvalid={(e) =>
+              		e.target.setCustomValidity('First name must contain 2 to 25 alphabetical characters.') } onChange={(e) => e.target.setCustomValidity('')} required/>
 			</div>
 			<div className="input-wrapper-lastname">
-				<span>Last Name: </span><input type="text" name="lastName" required pattern="^[A-Za-z]{2,30}$" onInvalid= {(e) => { e.target.setCustomValidity("Length has to be between 2 and 30 characters, without digits and symbols!"); e.target.setCustomValidity("");}}/> 
+				<span>Last Name: </span><input type="text" name="lastName" pattern="^[A-Za-z]{2,25}$" onInvalid={(e) =>
+              		e.target.setCustomValidity('First name must contain 2 to 25 alphabetical characters.') } onChange={(e) => e.target.setCustomValidity('')} required/> 
 			</div>
 			<div className="input-wrapper-phone">
-				<span>Phone: </span><input type="text" name="phone" required pattern="[0-9]{10}" onInvalid= {(e) => { e.target.setCustomValidity("The number has to be exactly 10 digits!"); e.target.setCustomValidity("");}}/> 
+				<span>Phone: </span><input type="text" name="phone" pattern="[0-9]{10}" onInvalid={(e) =>
+              		e.target.setCustomValidity('Phone must contain exactly 10 digits.') } onChange={(e) => e.target.setCustomValidity('')} required/> 
 			</div>
 			<div className="input-wrapper-email">
-				<span>E-mail: </span><input type="text" name="email" required pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(?:\.[a-zA-Z]{2,})?" onInvalid={(e) => { e.target.setCustomValidity("Please enter a valid email address"); e.target.setCustomValidity("");}}/> 
+				<span>E-mail: </span><input type="text" name="email" pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(?:\.[a-zA-Z]{2,})?"
+				 onInvalid={(e) => e.target.setCustomValidity('Invalid email format.') } onChange={(e) => e.target.setCustomValidity('')} required/> 
 			</div>
 		</div>
 		<div className="coutries-wrapper">
